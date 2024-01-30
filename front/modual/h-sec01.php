c33a9f230519f2db40eea81e21d7eef3b7ede4cb
Hanna <hannawang003@gmail.com>
2024-01-30 13:08:34 +0800

    優化多張圖片顯示數量及大小

--- front/modual/h-sec01.php
+++ front/modual/h-sec01.php
@@ -6,21 +6,31 @@
                 <span data-scroll data-scroll-direction="horizontal" data-scroll-speed="-2">ROCK</span> <span
                     data-scroll data-scroll-direction="horizontal" data-scroll-speed="1">ROMANTIC</span>
             </h1>
-            <p class="goto" data-to="#mainContainer"  data-scroll data-scroll-direction="horizontal" data-scroll-speed="2">Where from, where to</p>
+            <p class="goto" data-to="#mainContainer" data-scroll data-scroll-direction="horizontal"
+                data-scroll-speed="2">Where from, where to</p>
         </div>
         <div style="width:60vw;position:absolute;right:0;" class="d-flex flex-wrap">
             <?php
             // $total = $Gallery->count()-3;
             // $start = rand(0,$total);
-$photos=$Gallery->all();
-// dd($photos);
-// $top = 20;
-// $left = 20;
-foreach($photos as $photo){
-    echo "<div style='width:200px;height:200px;background:black;background:url(./img/{$photo['img']})'></div>";
-// $top+=300;
-// $left+=200;
-}
+            $total = $Gallery->count();
+            // echo $total;
+            if ($total <= 100) {
+                $start = 0;
+            } elseif (rand(0, $total) > ($total - 100)) {
+                $start = $total - 100;
+            } else {
+                $start = rand(0, $total);
+            }
+            $photos = $Gallery->all("limit $start,100");
+            // dd($photos);
+            // $top = 20;
+            // $left = 20;
+            foreach ($photos as $photo) {
+                echo "<div style='width:10vw;height:10vw;background:black;background:url(./img/{$photo['img']});background-size:cover;'></div>";
+                // $top+=300;
+                // $left+=200;
+            }
             ?>
 
 
@@ -29,7 +39,7 @@ foreach($photos as $photo){
     </div>
 </section>
 <script>
-                $('.goto').on('click', function() {
+$('.goto').on('click', function() {
     var target = $(this).attr('data-to');
     target = document.querySelector(target);
     console.log(target)
@@ -37,4 +47,4 @@ foreach($photos as $photo){
     $('#menu').hide();
 
 })
-            </script>
+</script>
